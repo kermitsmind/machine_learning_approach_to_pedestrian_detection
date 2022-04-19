62d0165ff09c7f5eaf925a393943d17881ab3836
kermitsmind <kermitsmind@icloud.com>
2022-04-19 13:37:12 +0200

    refactorized .ipynb and applied changes to .py also

--- Detector.py
+++ Detector.py
@@ -4,11 +4,14 @@ from detectron2.data import MetadataCatalog
 from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2 import model_zoo
 from detectron2.structures import Boxes
+from detectron2.structures import Keypoints
+from PIL import Image
 
 import detectron2
 import cv2 as cv2
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
 
 
 # based on: https://www.youtube.com/watch?v=Pb3opEFP94U&t=813s
@@ -18,24 +21,44 @@ class Detector:
     Processes the image.
     """
 
-    def __init__(self):
+    def __init__(self, mode):
         """
-        Constructor of the class.
+        Method initializing a detector with a config file
+
+        :param: mode: Select work mode (Object Detection, Keypoint Extraction)
+
+        :return predictor and cfg objects
         """
 
-        self.cfg = get_cfg()
+        if mode == "OD":
+            self.cfg_od = get_cfg()
+            self.cfg_od.merge_from_file(
+                model_zoo.get_config_file(
+                    "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
+                )
+            )
+            self.cfg_od.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+                "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
+            )
 
-        self.cfg.merge_from_file(
-            model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
-        )
-        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-            "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
-        )
+            self.cfg_od.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+            self.cfg_od.MODEL.DEVICE = "cpu"
+            self.predictor_od = DefaultPredictor(self.cfg_od)
 
-        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
-        self.cfg.MODEL.DEVICE = "cpu"
+        elif mode == "KP":
+            self.cfg_kp = get_cfg()
+            self.cfg_kp.merge_from_file(
+                model_zoo.get_config_file(
+                    "COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"
+                )
+            )
+            self.cfg_kp.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+                "COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"
+            )
 
-        self.predictor = DefaultPredictor(self.cfg)
+            self.cfg_kp.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+            self.cfg_kp.MODEL.DEVICE = "cpu"
+            self.predictor_kp = DefaultPredictor(self.cfg_kp)
 
     # based on: https://colab.research.google.com/drive/1x-eMvFQTLBTr7ho9ZlYkHF0NmyUyAlxT?usp=sharing#scrollTo=Z_pe5XFayoHP
     def chooseOneClassFromAllDetected(self, initialPredictions, image):
@@ -67,54 +90,139 @@ class Detector:
 
         return obj
 
-    def processImage(self, image, class_reduction=True, image_color_mode="IMAGE"):
+    def loadImage(self, imagePath, changeColorMode=False):
         """
-        Method responsible for performing actual detector usage on an image
+        Method responsible for loading an image
 
-        :param: image: image object to be processed
-        :param: class_reduction: decides if all classes will be used or only one
-        :param: image_color_mode: color mode for showing the results
+        :param: imagePath: path to the image
+        :param: changeColorMode: change BGR to RGB, useful when using both cv2 and matplotlib
 
-        return: image object
+        :return image object
         """
 
-        self.image = image
-        if class_reduction == True:
-            predictions = self.predictor(image)
+        image = cv2.imread(imagePath)
+        if changeColorMode == True:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        return image
+
+    def performDetection(self, predictor, image, classReduction):
+        """
+        Method performing actual detection
+
+        :param: predictor: predictor object
+        :param: image: image object
+        :param: classReduction: enables class reduction to one
+
+
+        :return prediction, (optionally) new_predictions and prediction boxes objects
+        """
+
+        predictions = predictor(image)
+
+        if classReduction == True:
             new_predictions = self.chooseOneClassFromAllDetected(
                 initialPredictions=predictions, image=image
             )
             prediction_boxes = [x.numpy() for x in list(new_predictions.pred_boxes)]
-        else:
-            new_predictions = self.predictor(image)
-            prediction_boxes = [
-                x.numpy() for x in list(new_predictions["instances"].pred_boxes)
-            ]
 
-        if image_color_mode == "IMAGE":
-            viz = Visualizer(
-                image[:, :, ::-1],
-                metadata=MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),
-                instance_mode=ColorMode.IMAGE,
-            )
-        elif image_color_mode == "IMAGE_BW":
-            viz = Visualizer(
-                image[:, :, ::-1],
-                metadata=MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),
-                instance_mode=ColorMode.IMAGE_BW,
-            )
-        elif image_color_mode == "SEGMENTATION":
-            viz = Visualizer(
-                image[:, :, ::-1],
-                metadata=MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),
-                instance_mode=ColorMode.SEGMENTATION,
-            )
+            return predictions, new_predictions, prediction_boxes
 
-        if class_reduction == True:
-            output = viz.draw_instance_predictions(new_predictions.to("cpu"))
-        else:
-            output = viz.draw_instance_predictions(
-                new_predictions["instances"].to("cpu")
-            )
+        elif classReduction == False:
 
-        return output.get_image()[:, :, ::-1], prediction_boxes
+            return predictions
+
+    def cropImageByBoundingBox(self, image, box):
+        """
+        Function responsible for cropping part of an image defined by bounding box coordinates
+
+        :param: image: image to be processed
+        :param: box: numpy array with four box coordinates
+
+        return: cropped image object
+        """
+
+        x_top_left = box[0]
+        y_top_left = box[1]
+        x_bottom_right = box[2]
+        y_bottom_right = box[3]
+
+        if type(image) == np.ndarray:
+            image = Image.fromarray(image)
+        crop_img = image.crop(
+            (int(x_top_left), int(y_top_left), int(x_bottom_right), int(y_bottom_right))
+        )
+
+        if type(image) == Image.Image:
+            crop_img = np.array(crop_img)
+
+        return crop_img, (x_top_left, y_top_left)
+
+    def chooseIndexOfBestKeypointInstanceFromAllDetected(self, predictions):
+        """
+        Method responsible for choosing the best (the longest in the sense of euclidean distance) detection instance
+
+        :param: predictions: predictions produced by self.predictor which is DefaultPredictor(self.cfg)
+
+        return index of the best (the longest in the sense of euclidean distance) prediction
+        """
+
+        # index = np.array([predictions["instances"][i].pred_keypoints.numpy().sum() for i in range(len(predictions["instances"].pred_keypoints.numpy()))])
+
+        length = len(predictions["instances"].pred_keypoints.numpy())
+        [
+            predictions["instances"][i].pred_keypoints.numpy().sum()
+            for i in range(length)
+        ]
+
+        initial_predictions_all = predictions["instances"].pred_keypoints.numpy()
+        index = np.array([])
+
+        for prediction in initial_predictions_all:
+            sum = 0
+            for i in range(len(prediction) - 1):
+                sum += np.linalg.norm(prediction[i] - prediction[i + 1])
+            index = np.append(index, sum)
+
+        index_of_value_to_keep = np.argmax(index)
+
+        return int(index_of_value_to_keep)
+
+    def correctIndividualSkeletonCoordinates(
+        self, initialPredictions, image, croppedImageCoordinates
+    ):
+        """
+        Method responsible for correction of individual detection (box, keypoints) coordinates to match initial picture position
+
+        :param: initialPredictions: predictions produced by self.predictor which is DefaultPredictor(self.cfg)
+        :param: image: cropped image
+        :param: croppedImageCoordinates: cooridnates of the cropped image on the initiial one
+
+        return corrected predictions (box, keypoints)
+        """
+
+        boxes = initialPredictions["instances"].pred_boxes
+        boxes_filtered = boxes.tensor.cpu().numpy().copy()
+        boxes_filtered[:, 0] += croppedImageCoordinates[0]
+        boxes_filtered[:, 2] += croppedImageCoordinates[0]
+        boxes_filtered[:, 1] += croppedImageCoordinates[1]
+        boxes_filtered[:, 3] += croppedImageCoordinates[1]
+
+        keypoints = initialPredictions["instances"].pred_keypoints
+        keypoints_filtered = keypoints.cpu().numpy().copy()
+        keypoints_filtered[:, :, 0] += croppedImageCoordinates[0]
+        keypoints_filtered[:, :, 1] += croppedImageCoordinates[1]
+
+        scores = initialPredictions["instances"].scores
+
+        boxes_filtered = Boxes(torch.tensor(boxes_filtered))
+        keypoints_filtered = torch.tensor(keypoints_filtered)
+
+        obj = detectron2.structures.Instances(
+            image_size=(image.shape[0], image.shape[1])
+        )
+        obj.set("pred_boxes", boxes_filtered)
+        obj.set("pred_keypoints", keypoints_filtered)
+        obj.set("scores", scores)
+
+        return obj
--- main.py
+++ main.py
@@ -1,24 +1,81 @@
 from Detector import *
-import matplotlib.pyplot as plt
 
 
 def main():
-    # initialize detector in OD mode
-    detector = Detector()
 
-    # load an image
+    # prepare detectors
+    ## initialize OD detector
+    detector_od = Detector("OD")
+    predictor_od = detector_od.predictor_od
+    cfg_od = detector_od.cfg_od
+
+    ## initialize KP detector
+    detector_kp = Detector("KP")
+    predictor_kp = detector_kp.predictor_kp
+    cfg_kp = detector_kp.cfg_kp
+
+    # 0. load an image
     imagePath = "images/Pedestrian-Deaths-India-Road-Fatalities-India-1280x720.jpeg"
-    image = cv2.imread(imagePath)
+    image = detector_od.loadImage(imagePath=imagePath, changeColorMode=False)
 
-    # process image with the detector
-    image, prediction_boxes = detector.processImage(
-        image=image, class_reduction=True, image_color_mode="IMAGE"
+    # 1. perform detection and make prediction boxes
+    (
+        predictions_od,
+        new_predictions_od,
+        prediction_od_boxes,
+    ) = detector_od.performDetection(
+        predictor=predictor_od, image=image, classReduction=True
+    )
+    ## make visualization part
+    viz_od = Visualizer(
+        image[:, :, ::-1],
+        metadata=MetadataCatalog.get(cfg_od.DATASETS.TRAIN[0]),
+        instance_mode=ColorMode.IMAGE,
+    )
+    output_od = viz_od.draw_instance_predictions(new_predictions_od.to("cpu"))
+    image_with_boxes = output_od.get_image()[:, :, ::-1]
+
+    # 2. cut part of the image from bounding boxes
+    image_index = 1
+    image_cropped, cropped_image_coordinates = detector_od.cropImageByBoundingBox(
+        image=image, box=prediction_od_boxes[image_index]
     )
 
-    print(prediction_boxes)
+    # 3. make individual skeletonization for the cropped image
+    ## perform detection
+    predictions_kp = detector_kp.performDetection(
+        predictor=predictor_kp, image=image_cropped, classReduction=False
+    )
 
-    # show the resuls
-    cv2.imshow("Result", image)
+    viz_kp = Visualizer(
+        image_cropped[:, :, ::-1],
+        metadata=MetadataCatalog.get(cfg_kp.DATASETS.TRAIN[0]),
+        instance_mode=ColorMode.IMAGE,
+    )
+    index = detector_kp.chooseIndexOfBestKeypointInstanceFromAllDetected(
+        predictions=predictions_kp
+    )
+    output_kp = viz_kp.draw_instance_predictions(
+        predictions_kp["instances"][index].to("cpu")
+    )
+    image_skeletonized = output_kp.get_image()[:, :, ::-1]
+
+    # 4. project individual mask on the initial picture
+    corrected_predictions = detector_kp.correctIndividualSkeletonCoordinates(
+        initialPredictions=predictions_kp,
+        image=image_cropped,
+        croppedImageCoordinates=cropped_image_coordinates,
+    )
+    viz_kp = Visualizer(
+        image[:, :, ::-1],
+        metadata=MetadataCatalog.get(cfg_kp.DATASETS.TRAIN[0]),
+        instance_mode=ColorMode.IMAGE,
+    )
+    output_kp = viz_kp.draw_instance_predictions(corrected_predictions[index].to("cpu"))
+    image_skeletonized = output_kp.get_image()[:, :, ::-1]
+
+    ## show the resuls
+    cv2.imshow("Result", image_skeletonized)
     cv2.waitKey()
 
 
